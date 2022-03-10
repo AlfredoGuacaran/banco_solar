f195bf5515b9826afb4318bbf62dfcf31185d68f
AlfredoGuacaran <alfredoguacaran21@gmail.com>
2022-03-09 23:01:51 -0300

    rutas, get, edit, delete, nueva transferencia 60%

--- db.js
+++ db.js
@@ -26,7 +26,7 @@ export async function nuevoUsuario(nombre, balance) {
     return res.rowCount && `Usuario ${nombre} registrado con exito`;
   } catch (error) {
     console.log(error);
-    throw error;
+    throw error.message;
   }
 }
 
@@ -35,10 +35,68 @@ export async function getUsuarios(nombre, balance) {
     const client = await pool.connect();
     const res = await client.query('select * from usuarios;');
     client.release();
-
     return res.rows;
   } catch (error) {
     console.log(error);
+    throw error;
+  }
+}
+
+export async function editarUsuario(id, nombre, balance) {
+  try {
+    const client = await pool.connect();
+    const res = await client.query({
+      text: 'update usuarios set nombre = $2, balance = $3 where id = $1',
+      values: [id, nombre, balance],
+    });
+    client.release();
+    if (res.rowCount == 1) return `Usuario ${nombre} editado con exito`;
+    else throw `Usuario ${nombre} no existe`;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function deleteUsuario(id) {
+  try {
+    const client = await pool.connect();
+    const res = await client.query({
+      text: 'DELETE from usuarios where id = $1;',
+      values: [id],
+    });
+    client.release();
+    return res.rowCount ? `Usuario eliminado con exito` : `Usuario no existe`;
+  } catch (error) {
+    console.log(error.message);
+    throw error;
+  }
+}
+
+export async function newTransferencia(emisor, receptor, monto) {
+  try {
+    const client = await pool.connect();
+    const { rows: datosUsuarios } = await client.query({
+      text: 'select * from usuarios where nombre in ($1,$2)',
+      values: [emisor, receptor],
+    });
+
+    const [datosEmisor, datosReceptor] = datosUsuarios;
+    if (monto > datosEmisor.balance)
+      throw new Error('Emisor no tiene suficiente saldo');
+
+    const newBalanceEmisor = await client.query({
+      text: 'update usuarios set balance = $2 where id=',
+      values: [datosEmisor.id, datosReceptor.id, monto],
+    });
+
+    const transferencia = await client.query({
+      text: 'insert into transferencias (fecha,emisor,receptor, monto) values (now(),$1,$2,$3)',
+      values: [datosEmisor.id, datosReceptor.id, monto],
+    });
+
+    client.release();
+  } catch (error) {
     throw error;
   }
 }
